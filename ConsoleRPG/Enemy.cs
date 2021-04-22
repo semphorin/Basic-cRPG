78d1f05f38d48b0742b82b6b81b9a60f34461b1f
calebdockal <64401265+calebdockal@users.noreply.github.com>
2021-04-22 04:32:11 -0400

    Item.cs and Enemy.cs Updated

--- ConsoleRPG/Enemy.cs
+++ ConsoleRPG/Enemy.cs
@@ -1,25 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace ConsoleRPG
 {
-    class Enemy
+    public class Enemy
     {
-        private string enemyname;
-        private string enemyrace;
-        private int enemyhealth;
-        // Allegiences are factions a particular entity can align themselves with
-        private string enemyallegience;
-
-    }
-
-    class Goblin : Enemy
-    {
-        Enemy goblin = new Enemy();
-
-    }
-
-    public void EnemyIntroduction()
-    {
- 
+        // Each of these are properties of the Enemy
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int CurrentHitPoints { get; set; }
+        public int Damage { get; set; }
     }
 }
